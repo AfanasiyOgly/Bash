bb9032e9c1f279aaf1e5599f05c8adf124917c19
AfanasiyOgly <125655074+AfanasiyOgly@users.noreply.github.com>
2024-01-29 21:53:22 +0900

    Grep
    Parsing grep с неработающей self_realloc

--- s21_grep.c
+++ s21_grep.c
@@ -2,118 +2,212 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+static const int OPTIONS_END = -1;
+static const int PATTERNS_INIT = 1000;
+static const int PATTERNS_ADD = 1000;
+static const int BUFFER_INIT = 1000;
+static const char *FOPEN_READ = "r";
+static const int BUFFER_MULT = 1000;
 
-typedef struct s21_grep
+struct Patterns
 {
-    int e;
-    int i;
-    int v;
-    int c;
-    int l;
-    int n;
-    int h;
-    int s;
-    int f;
-    int o;
+    char **data;
+    size_t cur_size;
+    size_t max_size;
+};
+typedef struct Patterns Patterns;
 
-} options;
-
-void get_options(int argc, char *argv[], options *opts, char **templates, int *temlates_count, char **templates_files, int *temlates_files_count)
+struct Options
 {
-    int opt;
-    static struct option long_options[] = {
-        /*длинных флагов нет поэтому зануляем лонг*/
-        {0, 0, 0, 0}};
-    int options_index;
-    while ((opt = (getopt_long(argc, argv, "e:ivclnhsf:o", long_options,
-                               &options_index))) !=
-           -1) /*установка коротких флагов в структуре*/
+    bool e;
+    bool i;
+    bool v;
+    bool c;
+    bool l;
+    bool n;
+    bool h;
+    bool s;
+    bool f;
+    bool o;
+    struct Patterns patts;
+};
+typedef struct Options Options;
+static const char SHORTOPTIONS[] = "e:f:isvnholc";
+static struct option LONGOPTIONS[] = {
+    {"regexp", required_argument, NULL, 'e'},
+    {"file", required_argument, NULL, 'f'},
+    {"ignore-case", no_argument, NULL, 'i'},
+    {"no-messages", no_argument, NULL, 's'},
+    {"invert-match", no_argument, NULL, 'v'},
+    {"line-number", no_argument, NULL, 'n'},
+    {"no-filename", no_argument, NULL, 'h'},
+    {"only-matching", no_argument, NULL, 'o'},
+    {"file-witch-matches", no_argument, NULL, 'l'},
+    {"count", no_argument, NULL, 'c'},
+    {"help", no_argument, NULL, 0},
+    {0, 0, 0, 0}};
+
+/*функция выделения и проверки памяти*/
+void *safe_malloc(const size_t size)
+{
+    void *ret_ptr = malloc(size);
+    if (ret_ptr == NULL)
     {
-        switch (opt)
+        printf("ERROR");
+        exit(1);
+    }
+    return ret_ptr;
+}
+/*функция добавления памяти*/
+void *safe_realloc(void *buffer, const size_t size)
+{
+    void *ret_ptr = realloc(buffer, size);
+    if (ret_ptr == NULL)
+    {
+        printf("ERROR no realloc\n");
+        exit(1);
+    }
+    return ret_ptr;
+}
+static void patterns_add(Patterns *const patts, const char *const patt)
+{
+    if (patts->cur_size == patts->max_size)
+    {
+        patts->max_size += PATTERNS_ADD;
+        patts->data = safe_realloc(patts->data, patts->max_size * sizeof(char *));
+    }
+    patts->data[patts->cur_size] = safe_malloc(sizeof(char) * strlen(patt) + sizeof(char));
+    strcpy(patts->data[patts->cur_size++], patt);
+}
+FILE *safe_fopen(const char *filename, const char *mode)
+{
+    FILE *file = fopen(filename, mode);
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+    return file;
+}
+static void buffer_file(FILE *file, char *buffer)
+{
+    size_t size = 0;
+    size_t max_size = BUFFER_INIT;
+    char simbol = fgetc(file);
+    while (!feof(file))
+    {
+        buffer[size++] = simbol;
+        simbol = fgetc(file);
+        if (size != max_size)
         {
-        case 'e':
-            opts->e = 1;
-            templates[*temlates_count] = optarg;
-            temlates_count++;
-            break;
-        case 'i':
-            opts->i = 1;
-            break;
-        case 'v':
-            opts->v = 1;
-            break;
-        case 'c':
-            opts->c = 1;
-            break;
-        case 'l':
-            opts->l = 1;
-            break;
-        case 'n':
-            opts->n = 1;
-            break;
-        case 'h':
-            opts->h = 1;
-            break;
-        case 's':
-            opts->s = 1;
-            break;
-        case 'f':
-            opts->f = 1;
-            templates_files[*temlates_files_count] = optarg;
-            temlates_files_count++;
-            break;
-        case 'o':
-            opts->o = 1;
-            break;
-        default:
-            printf("Error\n");
-            exit(1); /*выход из программы если неверный флаг*/
-            break;
+            max_size *= BUFFER_MULT;
+            buffer = safe_realloc(buffer, max_size * sizeof(char));
         }
     }
-    for (int i = 0; i < *temlates_files_count; i++)
+    buffer[size] = '\0';
+}
+static void patterns_add_from_string(Patterns *const patts, const char *const str)
+{
+    char *temp_patts = safe_malloc(sizeof(char) * strlen(str) + sizeof(char));
+    strcpy(temp_patts, str);
+    char *token = strtok(temp_patts, "\n");
+    while (token != NULL)
     {
-        FILE *file = NULL;
-        file = fopen(templates_files[i], "r");
-        if (file == NULL)
-        {
-            fprintf(stderr, "Error - file NULL");
-            exit(1); /*выход из программы если файл пустой*/
-        }
-        else
-            fclose(file);
+        patterns_add(patts, token);
+        token = strtok(NULL, "\n");
+    }
+    free(temp_patts);
+}
+static void patterns_add_from_file(Patterns *const patts, char *const filename)
+{
+    FILE *file = safe_fopen(filename, FOPEN_READ);
+    char *buffer = safe_malloc(sizeof(char) * BUFFER_INIT);
+    buffer_file(file, buffer);
+    patterns_add_from_string(patts, buffer);
+    free(buffer);
+    fclose(file);
+}
+
+static void set_options(const char option, Options *options)
+{
+    switch (option)
+    {
+    case 'e':
+        options->e = true;
+        patterns_add_from_string(&options->patts, optarg);
+        break;
+    case 'f':
+        options->f = true;
+        patterns_add_from_file(&options->patts, optarg);
+        break;
+    case 'i':
+        options->i = true;
+        break;
+    case 's':
+        options->s = true;
+        break;
+    case 'v':
+        options->v = true;
+        break;
+    case 'n':
+        options->n = true;
+        break;
+    case 'h':
+        options->h = true;
+        break;
+    case 'o':
+        options->o = true;
+        break;
+    case 'l':
+        options->l = true;
+        break;
+    case 'c':
+        options->c = true;
+        break;
+    default:
+        printf("Error\n");
+        exit(1);
+    }
+}
+
+/*Освобождение структуры patts*/
+static void patterns_free(Patterns *const patts)
+{
+    for (size_t i = 0; i < patts->cur_size; i++)
+    {
+        free(patts->data[i]);
+    }
+    free(patts->data);
+}
+static void options_free(Options *const opts) { patterns_free(&opts->patts); }
+static void patterns_init(Patterns *const patts)
+{
+    patts->cur_size = 0;
+    patts->max_size = PATTERNS_INIT;
+    patts->data = safe_malloc(sizeof(char *) * patts->max_size);
+}
+
+void get_options(int argc, char *argv[], Options *opts)
+{
+    patterns_init(&opts->patts);
+    int long_options_index = 0;
+    char opt = getopt_long(argc, argv, SHORTOPTIONS, LONGOPTIONS, &long_options_index);
+    while (opt != OPTIONS_END)
+    {
+        set_options(opt, opts);
+        opt = getopt_long(argc, argv, SHORTOPTIONS, LONGOPTIONS, &long_options_index);
     }
 }
 
 int main(int argc, char *argv[])
 {
-    char *templates[argc];
-    int temlates_count = 0;
-    char *templates_files[argc];
-    int temlates_files_count = 0;
-    options opts;
-    memset(&opts, 0, sizeof(opts)); // инициализация структуры нулями
-    get_options(argc, argv, &opts, templates, &temlates_count, templates_files, &temlates_files_count);
-    for (int i = 0; i < temlates_count; i++)
+    Options opts = {0};
+    get_options(argc, argv, &opts);
+    for (size_t i = 0; i < opts.patts.cur_size; i++)
     {
-        printf("templates = %s\n", templates[i]);
+        puts(opts.patts.data[i]);
     }
-    for (int i = 0; i < temlates_files_count; i++)
-    {
-        printf("temlaates_files = %s", templates_files[i]);
-    }
-    printf("templates_files_count = %d\n", temlates_files_count);
-    printf("templates_count = %d\n", temlates_count);
-    printf("e = %d\n", opts.e);
-    printf("i = %d\n", opts.i);
-    printf("v = %d\n", opts.v);
-    printf("c = %d\n", opts.c);
-    printf("l = %d\n", opts.l);
-    printf("n = %d\n", opts.n);
-    printf("h = %d\n", opts.h);
-    printf("s = %d\n", opts.s);
-    printf("f = %d\n", opts.f);
-    printf("o = %d\n", opts.o);
-
+    options_free(&opts);
     return 0;
 }
